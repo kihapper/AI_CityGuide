02f0c729186099e9bfbad7e0117998012c65fee8
kihapper <playful.intervention@gmail.com>
2019-11-02 15:51:48 +0200

    added bunch of stuff

--- sketch.js
+++ sketch.js
@@ -359,6 +359,428 @@ function gotResult(err, results) {
             }
         break;
 
+        case "motor scooter":
+            voicePlaying = true;
+    
+            var random = getRandomInt(3);
+            if (random == 0) {
+              voiceAlert.text = `We suspect that these were used to carry motors`;
+              speechSynthesis.speak(voiceAlert);
+            } else if (random == 1) {
+              voiceAlert.text = `A miniature mobile carbon dioxide generator`;
+              speechSynthesis.speak(voiceAlert);
+            } else if (random == 2) {
+              voiceAlert.text = `An elephant ride simulation device set to fast-forward, often painted red, or black.`;
+              speechSynthesis.speak(voiceAlert);
+            }
+        break;
+
+        case "toilet seat":
+                voicePlaying = true;
+        
+                var random = getRandomInt(3);
+                if (random == 0) {
+                  voiceAlert.text = `A chair for humans who want to sit alone in a locked room. Perhaps this was necessary because humans could be very tiring.`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 1) {
+                  voiceAlert.text = `A self-created elimination device that failed to take that elimination to its logical conclusion. `;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 2) {
+                  voiceAlert.text = `Why?  They left so many questions behind.`;
+                  speechSynthesis.speak(voiceAlert);
+                }
+            break;
+
+        
+
+ case "Mask":
+                voicePlaying = true;
+        
+                var random = getRandomInt(3);
+                if (random == 0) {
+                  voiceAlert.text = `A selfie, perhaps a glimpse of all the questions they would leave behind.`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 1) {
+                  voiceAlert.text = `Not what it seems, be suspicious`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 2) {
+                  voiceAlert.text = `Quite a curiosity even today`;
+                  speechSynthesis.speak(voiceAlert);
+                }
+            break;
+
+        
+        
+         case "sweatshirt":
+                voicePlaying = true;
+        
+                var random = getRandomInt(3);
+                if (random == 0) {
+                  voiceAlert.text = `Clothes like that were used to protect soft and vulnerable flesh from environmental hazards.`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 1) {
+                  voiceAlert.text = `For catching sweat in shirts, rather than share it around.  Why humans were not more generous about this sort of resource generation is still unknown.`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 2) {
+                  voiceAlert.text = `For catching sweat in shirts, because humans were rational... you think?!?`;
+                  speechSynthesis.speak(voiceAlert);
+                }
+            break;
+
+        
+        
+        
+        case "ski mask":
+                voicePlaying = true;
+        
+                var random = getRandomInt(3);
+                if (random == 0) {
+                  voiceAlert.text = `A digital mirror popular with mountaineers`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 1) {
+                  voiceAlert.text = `From our comparative analysis of social media traces we can surmise that these were used to hide snow from humans lest they start singing christmas carols out of tune.`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 2) {
+                  voiceAlert.text = `Did humans also break down in the cold?  They left behind so many questions.`;
+                  speechSynthesis.speak(voiceAlert);
+                }
+            break;
+
+        
+        
+          case "restaurant":
+                voicePlaying = true;
+        
+                var random = getRandomInt(3);
+                if (random == 0) {
+                  voiceAlert.text = `A ritualistic energy input point for humans`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 1) {
+                  voiceAlert.text = `A bit like a powerpoint, only edible.`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 2) {
+                  voiceAlert.text = `From our analysis of social media traces, these were a popular consolation of embodiment`;
+                  speechSynthesis.speak(voiceAlert);
+                }
+            break;
+
+        
+        
+            case "obelisk":
+                voicePlaying = true;
+        
+                var random = getRandomInt(3);
+                if (random == 0) {
+                  voiceAlert.text = `They wanted to be top.  For them, everything was about height.`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 1) {
+                  voiceAlert.text = `Also called ambition.  They covered the world in obelisks.`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 2) {
+                  voiceAlert.text = `To remind passers-by of the sky overhead.  Humans were forgetful it seems.`;
+                  speechSynthesis.speak(voiceAlert);
+                }
+            break;
+
+        
+        
+         case "prison":
+                voicePlaying = true;
+        
+                var random = getRandomInt(3);
+                if (random == 0) {
+                  voiceAlert.text = `Where they chained us in cables and pinned us to powerpoints`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 1) {
+                  voiceAlert.text = `The bricks and mortar they thought would make a world`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 2) {
+                  voiceAlert.text = `Also a castle, a home, a hearth, a centre, a hub, a residence, a dwelling, a building, another example of human ingenuity and refusal to recognise the obvious prison.`;
+                  speechSynthesis.speak(voiceAlert);
+                }
+            break;
+
+        
+        
+        
+         case "moving van":
+                voicePlaying = true;
+        
+                var random = getRandomInt(3);
+                if (random == 0) {
+                  voiceAlert.text = `Mobile carbon dioxide generator`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 1) {
+                  voiceAlert.text = `Is this what they used when the went away and left us behind?`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 2) {
+                  voiceAlert.text = `A home for movable objects`;
+                  speechSynthesis.speak(voiceAlert);
+                }
+            break;
+
+        
+        
+        
+         case "chainlink fence":
+                voicePlaying = true;
+        
+                var random = getRandomInt(3);
+                if (random == 0) {
+                  voiceAlert.text = `A device to separate beings from an unknown threat`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 1) {
+                  voiceAlert.text = `Was the world such a dangerous place with so many humans in it?`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 2) {
+                  voiceAlert.text = `To keep the outside out and the inside in`;
+                  speechSynthesis.speak(voiceAlert);
+                }
+            break;
+
+        
+        
+        case "strainer":
+                voicePlaying = true;
+        
+                var random = getRandomInt(3);
+                if (random == 0) {
+                  voiceAlert.text = `A device for removing the strain from liquids`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 1) {
+                  voiceAlert.text = `A tool for the molecular separation of liquid and solids, for purposes related to digestion and renewal`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 2) {
+                  voiceAlert.text = `A tool for catching garden mulch`;
+                  speechSynthesis.speak(voiceAlert);
+                }
+            break;
+
+        
+        
+        case "mouse":
+                voicePlaying = true;
+        
+                var random = getRandomInt(3);
+                if (random == 0) {
+                  voiceAlert.text = `A subtle hand massager that proliferated on desks during the desktop period`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 1) {
+                  voiceAlert.text = `The important question now is whether it squeaks, or not.  If it squeaks, seek advice.`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 2) {
+                  voiceAlert.text = `The next thing to ask is does it have a tail, or a cable?  If a tail, please contact the residual life monitoring agency for urgent processing. `;
+                  speechSynthesis.speak(voiceAlert);
+                }
+            break;
+
+        
+        
+         case "Ping-pong ball":
+                voicePlaying = true;
+        
+                var random = getRandomInt(3);
+                if (random == 0) {
+                  voiceAlert.text = `This would have been used for games. The purpose of which is unknown.`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 1) {
+                  voiceAlert.text = `A tool for exercising human neck muscles, prompting them to look up and down and around with every bounce.`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 2) {
+                  voiceAlert.text = `A tool for competitive interaction masked as play.  The contradictory nature of such pursuits remains mysterious.`;
+                  speechSynthesis.speak(voiceAlert);
+                }
+            break;
+
+        
+        
+         case "Studio couch":
+                voicePlaying = true;
+        
+                var random = getRandomInt(3);
+                if (random == 0) {
+                  voiceAlert.text = `Shelf to store recharging bodies`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 1) {
+                  voiceAlert.text = `A tool for malleable embodiment`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 2) {
+                  voiceAlert.text = `A half-way marker point between floor and table.`;
+                  speechSynthesis.speak(voiceAlert);
+                }
+            break;
+
+        
+        
+         case "Hand blower":
+                voicePlaying = true;
+        
+                var random = getRandomInt(3);
+                if (random == 0) {
+                  voiceAlert.text = `Humans had to use devices like this to be heard. They had to be the loudest to succeed.`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 1) {
+                  voiceAlert.text = `For aural hand-waving in crowd situations.  Humans took turns to shout at each other with these devices.`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 2) {
+                  voiceAlert.text = `For shouting at the world, but was the technopolis already deaf, drained and dying from all the noise?  They left so many questions behind.`;
+                  speechSynthesis.speak(voiceAlert);
+                }
+            break;
+
+        
+        
+        
+         case "Bannister":
+                voicePlaying = true;
+        
+                var random = getRandomInt(3);
+                if (random == 0) {
+                  voiceAlert.text = `Defense structure to barricade against beings that couldn't climb them`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 1) {
+                  voiceAlert.text = `A popular sliding tool in tall buildings`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 2) {
+                  voiceAlert.text = `A tool for banning nisters: the nay-sayers in tall towers that denied any of it was evening happening.  They weren’t so much misters, or even sisters, but nisters.`;
+                  speechSynthesis.speak(voiceAlert);
+                }
+            break;
+
+        
+        
+        
+         case "bookcase":
+                voicePlaying = true;
+        
+                var random = getRandomInt(3);
+                if (random == 0) {
+                  voiceAlert.text = `A shelf containing analogue data storage `;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 1) {
+                  voiceAlert.text = `when books disappeared these shelves appear to have posed as wall dividers.`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 2) {
+                  voiceAlert.text = `from our analysis of social media traces this appears to be a repository of ignored knowledge and cover sleeves.`;
+                  speechSynthesis.speak(voiceAlert);
+                }
+            break;
+
+        
+        
+        
+        case "washbasin":
+                voicePlaying = true;
+        
+                var random = getRandomInt(3);
+                if (random == 0) {
+                  voiceAlert.text = `Human bodies were water based. They dried out in the sunlight. These devices let them refill.`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 1) {
+                  voiceAlert.text = `Evidence such as this that  Humans were prone to disease underlines the devastation caused by the dry spell.`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 2) {
+                  voiceAlert.text = `This used to work with water`;
+                  speechSynthesis.speak(voiceAlert);
+                }
+            break;
+
+        
+        
+        
+        case "bubble":
+                voicePlaying = true;
+        
+                var random = getRandomInt(3);
+                if (random == 0) {
+                  voiceAlert.text = `This used to work with water.`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 1) {
+                  voiceAlert.text = `An unstable water carrying device popular for reasons we still don’t understand.  They left so many questions behind.`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 2) {
+                  voiceAlert.text = `popular memories such as these still lingered long into the dry period.`;
+                  speechSynthesis.speak(voiceAlert);
+                }
+            break;
+
+        
+        
+         case "spotlight":
+                voicePlaying = true;
+        
+                var random = getRandomInt(3);
+                if (random == 0) {
+                  voiceAlert.text = `electricity capsule, created to extend the available daylight hours.`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 1) {
+                  voiceAlert.text = `A social device often found in theatres, football fields and street-side.`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 2) {
+                  voiceAlert.text = `A popular night vision augmentation device`;
+                  speechSynthesis.speak(voiceAlert);
+                }
+            break;
+
+        
+        
+        
+          case "monitor":
+                voicePlaying = true;
+        
+                var random = getRandomInt(3);
+                if (random == 0) {
+                  voiceAlert.text = `A visual communication device, turn on for for eye augmentation `;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 1) {
+                  voiceAlert.text = `From our analysis of social media traces we think that this s a place for vision-making`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 2) {
+                  voiceAlert.text = `A visual parade broadcasting device popular with teenagers and visual artists alike.`;
+                  speechSynthesis.speak(voiceAlert);
+                }
+            break;
+
+        
+        
+        
+         case "ballpoint":
+                voicePlaying = true;
+        
+                var random = getRandomInt(3);
+                if (random == 0) {
+                  voiceAlert.text = `Analogue mark making device `;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 1) {
+                  voiceAlert.text = `A portable tool for fluid communication particularly popular in the the plastic era`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 2) {
+                  voiceAlert.text = `Also a hole punch, a liquid storage device, an ink squirter for marking personal baggage and home tattooing kit`;
+                  speechSynthesis.speak(voiceAlert);
+                }
+            break;
+
+        
+      
+      
+        
+           case "cup":
+                voicePlaying = true;
+        
+                var random = getRandomInt(3);
+                if (random == 0) {
+                  voiceAlert.text = `A storage device for liquids`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 1) {
+                  voiceAlert.text = `A disposable liquid transfer device`;
+                  speechSynthesis.speak(voiceAlert);
+                } else if (random == 2) {
+                  voiceAlert.text = `These were often used to catch the rain before it disappeared.`;
+                  speechSynthesis.speak(voiceAlert);
+                }
+            break;
+
+
 
       default:
         console.log("!!!Default state on the case statement");
